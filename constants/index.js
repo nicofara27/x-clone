b3b42e9b02a17c171687f445262b7c4e3fb9ad62
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-10-25 18:42:35 -0300

    Agregue links del menu

---
+++ constants/index.js
@@ -0,0 +1,27 @@
+export const menuLinks = [
+    {
+        nombre: "Inicio",
+        url: "/",
+        logo: "/assets/home.svg"
+    },
+    {
+        nombre: "Explorar",
+        url: "/explorar",
+        logo: "/assets/search.svg"
+    },
+    {
+        nombre: "Notificaciones",
+        url: "/notificaciones",
+        logo: "/assets/bell.svg"
+    },
+    {
+        nombre: "Listas",
+        url: "/listas",
+        logo: "/assets/list.svg"
+    },
+    {
+        nombre: "Perfil",
+        url: "/:user",
+        logo: "/assets/user.svg"
+    },
+]
