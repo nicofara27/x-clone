daf415a320dc14123e05849aaa62ca5fd2dc89e7
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-10-25 11:22:08 -0300

    Cree la estructura general

---
+++ components/common/LeftSideBar.tsx
@@ -0,0 +1,9 @@
+const LeftSideBar = () => {
+    return (
+        <div>
+            L-Bar
+        </div>
+    );
+};
+
+export default LeftSideBar;
---
+++ components/common/RightSideBar.tsx
@@ -0,0 +1,9 @@
+const RightSideBar = () => {
+    return (
+        <div>
+            R-Bar
+        </div>
+    );
+};
+
+export default RightSideBar;
