eb3fd45ee40bd968c3a9c36cf3b4b85b3b7da604
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-10-25 12:02:11 -0300

    Estructura basica del header agregada

--- components/common/Menu.tsx
+++ components/common/Menu.tsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Menu = () => {
     return (
-        <div>
-          Menu  
-        </div>
+        <nav className="fixed top-0 z-30 w-full flex items-center justify-between px-4 py-2">
+          <button className="md:hidden w-[33%] text-start">
+            usuario
+          </button>
+          <Link href="/" >
+            <Image src="/assets/logo.svg" alt="Logo" width={72} height={72} />
+          </Link>
+          <div className="w-[33%]">
+          </div>
+        </nav>
     );
 };
 
