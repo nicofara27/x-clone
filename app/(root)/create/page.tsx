2b4d563911f580704137b3c6c3867ded6c4a0363
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-11-06 18:28:32 -0300

    Agregue pagina temporal para subir posts

---
+++ app/(root)/create/page.tsx
@@ -0,0 +1,22 @@
+import Post from "@/components/forms/Post";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+  
+  return (
+    <>
+      <h1 className='head-text'>Create Thread</h1>
+      <Post  userId={userInfo._id} userImg={userInfo.img}/>
+    </>
+  );
+}
+
+export default Page;
