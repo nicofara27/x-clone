9f5623ca562749a62b662823c1fb98bf5263f2f4
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-11-07 11:19:31 -0300

    Agregue el llamado y el maquetado de los posts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,37 @@
-import Image from 'next/image'
+import PostCard from "@/components/cards/PostCard";
+import { fetchPosts } from "@/lib/actions/post.actions";
+import { currentUser } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const result = await fetchPosts();
   return (
     <>
-      <h1>home</h1>
+      <h1 className="head-text text-left">Home</h1>
+
+      <section className="mt-9 flex flex-col border-t">
+        {result.length === 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {result.map((post) => (
+              <PostCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </>
-  )
+  );
 }
