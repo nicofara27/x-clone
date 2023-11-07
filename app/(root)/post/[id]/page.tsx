627abbccefbd27132d4b2541e3cee9819244c2b1
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-11-07 11:19:57 -0300

    Agregue el llamado y el maquetado del post por id

---
+++ app/(root)/post/[id]/page.tsx
@@ -0,0 +1,38 @@
+import PostCard from "@/components/cards/PostCard";
+import { fetchPostsById } from "@/lib/actions/post.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
+
+async function Page({ params }: { params: { id: string } }) {
+  if (!params.id) return null;
+
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const post = await fetchPostsById(params.id);
+
+  return (
+    <section className="relative">
+      <div>
+        <PostCard
+          key={post._id}
+          id={post._id}
+          currentUserId={user?.id || ""}
+          parentId={post.parentId}
+          content={post.text}
+          author={post.author}
+          community={post.community}
+          createdAt={post.createdAt}
+          comments={post.children}
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Page;
