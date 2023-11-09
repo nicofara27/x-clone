2d9cad287a55a5e0b3555d47a6ef3444fa839d0a
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-11-09 18:24:34 -0300

    Movi componentes de la carpeta common a shared

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -1,6 +1,6 @@
 const RightSideBar = () => {
     return (
-        <div className="w-[35%]">
+        <div className="hidden md:block w-[10%] lg:w-[35%] border-l">
             R-Bar
         </div>
     );
