75cc399f3ceb223a724465c32493c19ff70cfc8b
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-11-13 10:16:18 -0300

    Agregue condicional que redirecciona al onboarding si es que el usuario todavia no finalizo de completar todos los datos

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,15 @@
 import PostCard from "@/components/cards/PostCard";
 import { fetchPosts } from "@/lib/actions/post.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const user = await currentUser();
   if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
   const result = await fetchPosts();
   return (
