37c999fe0e733110fffa759dfdaf1aad64512b19
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-11-13 10:20:10 -0300

    Agregue funcion para traer los post de un usuario en particular

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,46 +3,47 @@
 import { revalidatePath } from "next/cache"
 import User from "../models/user.model"
 import { connectToDB } from "../mongoose"
+import Post from "../models/post.model";
 
 interface Params {
-    userId: string;
-    username: string;
-    name: string;
-    bio: string;
-    img: string;
-    path: string;
+  userId: string;
+  username: string;
+  name: string;
+  bio: string;
+  img: string;
+  path: string;
 }
 export async function updateUser({
-    userId,
-    bio,
-    name,
-    path,
-    username,
-    img,
-  }: Params): Promise<void> {
-    try {
-      connectToDB();
-  
-      await User.findOneAndUpdate(
-        { id: userId },
-        {
-          username: username.toLowerCase(),
-          name,
-          bio,
-          img,
-          onboarded: true,
-        },
-        { upsert: true }
-      );
-  
-      if (path === "/profile/edit") {
-        revalidatePath(path);
-      }
-    } catch (error: any) {
-      throw new Error(`Error al crear o editar el usuario: ${error.message}`);
+  userId,
+  bio,
+  name,
+  path,
+  username,
+  img,
+}: Params): Promise<void> {
+  try {
+    connectToDB();
+
+    await User.findOneAndUpdate(
+      { id: userId },
+      {
+        username: username.toLowerCase(),
+        name,
+        bio,
+        img,
+        onboarded: true,
+      },
+      { upsert: true }
+    );
+
+    if (path === "/profile/edit") {
+      revalidatePath(path);
     }
+  } catch (error: any) {
+    throw new Error(`Error al crear o editar el usuario: ${error.message}`);
   }
-  
+}
+
 export async function fetchUser(userId: string) {
   try {
     connectToDB();
@@ -50,5 +51,31 @@ export async function fetchUser(userId: string) {
     return await User.findOne({ id: userId });
   } catch (error) {
     throw new Error(`Error al traer el usuario: ${error}`);
+  }
+}
+
+export async function fetchUserPosts(userId: string) {
+
+  try {
+    connectToDB();
+
+    const posts = await User.findOne({ id: userId })
+      .populate({
+        path: "posts",
+        model: Post,
+        populate: ({
+          path: "children",
+          model: Post,
+          populate: ({
+            path: "author",
+            model: User,
+            select: "id name img"
+          })
+        })
+      })
+
+      return posts;
+  } catch (error) {
+    throw new Error(`Error al traer los posts del usuario: ${error}`);
   }
 }
