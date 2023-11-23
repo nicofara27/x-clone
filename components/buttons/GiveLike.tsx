ced7d89fb448528fb02d7452f8695056774ecfa7
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-11-23 10:27:34 -0300

    Correccion en la funcion para dar like y agregue el numero de likes al lado del icono

--- components/buttons/GiveLike.tsx
+++ components/buttons/GiveLike.tsx
@@ -2,19 +2,37 @@
 import { addLike } from "@/lib/actions/post.actions";
 import Image from "next/image";
 
-const GiveLike = ({ postId, userId }: { postId: string; userId: string }) => {
-  const giveLike = async (postId: string, userId: string) => {
-    await addLike(JSON.parse(postId), JSON.parse(userId));
+const GiveLike = ({
+  postId,
+  userId,
+  postLikes,
+}: {
+  postId: string;
+  userId: string;
+  postLikes: any;
+}) => {
+  const uId = JSON.parse(userId);
+  const pId = JSON.parse(postId);
+  const pLikes = JSON.parse(postLikes);
+
+  const giveLike = async (pId: string, uId: string) => {
+    await addLike(pId, uId)
   };
+
   return (
-    <Image
-      className="cursor-pointer rounded-full p-1 hover:bg-sky-500 duration-200"
-      src="/assets/likes.svg"
-      alt="likes"
-      width={28}
-      height={28}
-      onClick={() => giveLike(postId, userId)}
-    />
+    <div className="flex items-center gap-1">
+      <Image
+        className={`cursor-pointer rounded-full p-1 ${
+          pLikes.likes.includes(userId) && "bg-pink-500"
+        } hover:bg-pink-500 duration-200`}
+        src="/assets/likes.svg"
+        alt="likes"
+        width={28}
+        height={28}
+        onClick={() => giveLike(pId, uId)}
+      />
+      <p className="text-xs">{pLikes.likes.length}</p>
+    </div>
   );
 };
 
