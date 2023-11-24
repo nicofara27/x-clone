b49992c4043b72958da721372e2186a1a30a7794
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-11-24 19:39:04 -0300

    Agregue funcion para seguir y dejar de seguir usuarios

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -10,18 +10,22 @@ async function Page({ params }: { params: { id: string } }) {
   const user = await currentUser();
   if (!user) return null;
 
-  const userInfo = await fetchUser(params.id);
+  const userProfileInfo = await fetchUser(params.id);
+  const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
-  
+  const isFollowing = userInfo.following.includes(userProfileInfo._id);
+  console.log(isFollowing)
+
   return (
     <>
       <ProfileHeader
-        accountId={params.id}
-        authUserId={user.id}
-        name={userInfo.name}
-        username={userInfo.username}
-        img={userInfo.img}
-        bio={userInfo.bio}
+        profileUserId={JSON.stringify(userProfileInfo._id)}
+        currentUserId={JSON.stringify(userInfo._id)}
+        name={userProfileInfo.name}
+        username={userProfileInfo.username}
+        img={userProfileInfo.img}
+        bio={userProfileInfo.bio}
+        isFollowing={isFollowing}
       />
       <div>
         <Tabs defaultValue="posts" className="mt-4 border-b">
@@ -34,7 +38,7 @@ async function Page({ params }: { params: { id: string } }) {
             </TabsList>
             {profileTabs.map((tab) => (
                 <TabsContent key={`tab-${tab.label}`} value={tab.value} className="mt-0">
-                    <PostsTab currentUserId={userInfo._id} accountId={userInfo.id} accountType="user"/>
+                    <PostsTab currentUserId={userProfileInfo._id} accountId={userProfileInfo.id} accountType="user"/>
                 </TabsContent>
             ))}
         </Tabs>
--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -1,15 +1,36 @@
+"use client";
 import Image from "next/image";
 import { Button } from "../ui/button";
+import { followUser, unfollowUser } from "@/lib/actions/user.actions";
+import { usePathname } from "next/navigation";
 
 interface Props {
-    accountId: string;
-    authUserId: string;
-    name: string;
-    username: string;
-    img: string;
-    bio: string;
+  profileUserId: string;
+  currentUserId: string;
+  name: string;
+  username: string;
+  img: string;
+  bio: string;
+  isFollowing: boolean;
 }
-const ProfileHeader = ({ accountId, authUserId, name, username, img, bio }:Props) => {
+const ProfileHeader = ({
+  profileUserId,
+  currentUserId,
+  name,
+  username,
+  img,
+  bio,
+  isFollowing,
+}: Props) => {
+  const path = usePathname();
+  const follow = async () => {
+    if(!isFollowing) {
+      await followUser(JSON.parse(currentUserId), JSON.parse(profileUserId), path);
+    } else {
+      console.log("first")
+      await unfollowUser(JSON.parse(currentUserId), JSON.parse(profileUserId), path);
+    }
+  };
   return (
     <div className="w-full">
       <div className="relative">
@@ -17,15 +38,27 @@ const ProfileHeader = ({ accountId, authUserId, name, username, img, bio }:Props
           <Image src={img} alt="Profile" fill />
         </div>
         <div className="relative h-32 w-32 -right-4 top-32">
-          <Image className="rounded-full border-4 border-gray-100 " src={img} alt="Profile" fill />
+          <Image
+            className="rounded-full border-4 border-gray-100 "
+            src={img}
+            alt="Profile"
+            fill
+          />
         </div>
       </div>
       <div className="flex justify-end mt-20 me-4">
-        <Button className=" bg-gray-100 rounded-full text-[#111] font-semibold hover:bg-gray-200">Seguir</Button>
+        <Button
+          className={`rounded-full font-semibold ${
+            isFollowing
+              ? "before:content-['Siguiendo'] hover:before:content-['Dejar'] bg-[#000] border w-[144px] hover:bg-[#190304] hover:text-red-500 hover:border-red-500 "
+              : "before:content-['Seguir'] bg-gray-100 hover:bg-gray-200 text-[#111]"
+          }`}
+          onClick={follow}
+        ></Button>
       </div>
       <div className="mt-8 mx-4">
         <h2 className="text-xl font-bold">{name}</h2>
-        <p className="text-sm text-gray-500">@{username}</p>  
+        <p className="text-sm text-gray-500">@{username}</p>
       </div>
       <div className="mt-4 mx-4">
         <p>{bio}</p>
