31040859634177efa1a048a4bdc0af9b015acf45
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-12-07 19:11:19 -0300

    Cambio para que se muestre la fecha de creacion del post o repost correctamente

--- components/shared/PostsTab.tsx
+++ components/shared/PostsTab.tsx
@@ -14,21 +14,28 @@ const PostsTab = async ({
   tab,
 }: Props) => {
   const result = await fetchUserPosts(accountId, tab);
+  function compareByDate(a: { createdAt: number }, b: { createdAt: number }) {
+    return a.createdAt - b.createdAt;
+  }
   let posts = result.posts.concat(result.reposts);
 
-    switch (tab) {
+  switch (tab) {
     case "replies": {
-      posts = posts.concat(result.comments);
+      posts = posts.concat(result.comments).sort(compareByDate).reverse();
+    } break;
+    case "posts": {
+      posts = posts.sort(compareByDate).reverse();
     }
   }
+
   return (
     <section>
       {posts.map((post: any, index: number) => (
         <PostCard
-          key={post._id}
-          id={post._id}
+          key={post._id || post.repost._id}
+          id={post._id || post.repost._id}
           userId={currentUserId}
-          content={post.text}
+          content={post.text || post.repost.text}
           author={
             accountType === "user"
               ? {
@@ -42,13 +49,19 @@ const PostsTab = async ({
                   img: post.author.image,
                   id: post.author.id,
                   username: post.author.username,
-                }
+                } ||
+              {
+                  name: post.repost.author.name,
+                  img: post.repost.author.image,
+                  id: post.repost.author.id,
+                  username: post.repost.author.username,
+                } 
           }
-          likes={post.likes}
-          reposts={post.reposts}
-          stats={post.stats}
-          createdAt={post.createdAt}
-          comments={post.children}
+          likes={post.likes || post.repost.likes}
+          reposts={post.reposts || post.repost.reposts}
+          stats={post.stats || post.repost.stats}
+          createdAt={post.repostedAt || post.createdAt}
+          comments={post.children || post.repost.children}
         />
       ))}
     </section>
