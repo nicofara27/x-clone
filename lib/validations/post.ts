a61fa631fdacbb1909ec663af15f1600b48685c7
Nicolas Fara <nicolas.fara.romero@gmail.com>
2024-02-12 11:47:49 -0300

    Agregue diferentes tipo de media a la validacion del post de forma opcional

--- lib/validations/post.ts
+++ lib/validations/post.ts
@@ -3,6 +3,9 @@ import * as z from "zod";
 
 export const PostValidation = z.object({
     post: z.string().min(3, { message: "Minimum 3 characters." }),
+    media: z.string().url().optional(),
+    gif: z.string().url().optional(),
+    emoji: z.string().optional(),
     accountId: z.string(),
   });
   
